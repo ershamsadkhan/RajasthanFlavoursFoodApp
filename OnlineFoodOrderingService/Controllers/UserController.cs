9327ca095f3768ac595d67fae623a564f582841e
ershamsadkhan <ershamsadkhan@gmail.com>
2017-04-23 20:18:26 +0530

    swagger added

---
+++ OnlineFoodOrderingService/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using OnlineFoodOrderingService.DTO;
+using OnlineFoodOrderingService.DTO.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineFoodOrderingService.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        [Route("SignUp")]
+        [HttpPost]
+        public Response<UserDto> SignUp(Request<UserDto> request)
+        {
+            Response<UserDto> response = new Response<UserDto>();
+            return response;
+        }
+
+        [Route("LogIn")]
+        [HttpPost]
+        public Response<UserDto> LogIn(Request<UserDto> request)
+        {
+            Response<UserDto> response = new Response<UserDto>();
+            return response;
+        }
+    }
+}
--- OnlineFoodOrderingService/DTO/User/UserDto.cs
+++ OnlineFoodOrderingService/DTO/User/UserDto.cs
@@ -7,5 +7,8 @@ namespace OnlineFoodOrderingService.DTO.User
 {
     public class UserDto
     {
+        public long Userid { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
     }
 }
